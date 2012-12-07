ca2b7b6a69671439db5259296c210cb9f7b1c0e0
Yutaka HARA <yutaka.hara.gmail.com>
2012-12-07 19:02:01 +0900

    added moon

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ App.sun = Ember.Object.create({
 
   draw: function(){
     this.set('circle', paper.circle(this.get('x'), this.get('y'), 8)
-                            .attr("fill", "#f00").attr("stroke", "#f00"));
+                            .attr("fill", "#f00").attr("stroke", "#000"));
   }
 });
 
@@ -36,22 +36,48 @@ App.earth = Ember.Object.create({
 
   draw: function(){
     this.set('circle', paper.circle(this.get('x'), this.get('y'), 5)
-                            .attr("fill", "#00f").attr("stroke", "#00f"));
+                            .attr("fill", "#00f").attr("stroke", "#000"));
   },
   move: function(){
     this.set('rot', this.get('rot') + 0.005);
   }
 });
 
-//App.moon = Ember.Object.create({
-//  earthRotBinding: 'App.earth.rot'
-//});
+App.moon = Ember.Object.create({
+  earthXBinding: 'App.earth.x',
+  earthYBinding: 'App.earth.y',
+  rot: 0,
+  circle: null,
+
+  x: function(){
+    return this.get('earthX') + 10 * Math.cos(this.get('rot'));
+  }.property('earthX', 'rot'),
+
+  y: function(){
+    return this.get('earthY') + 10 * Math.sin(this.get('rot'));
+  }.property('earthY', 'rot'),
+
+  onMove: function(){
+    this.get('circle').attr('cx', this.get('x'))
+                      .attr('cy', this.get('y'));
+  }.observes('rot', 'earthX'),
+
+  draw: function(){
+    this.set('circle', paper.circle(this.get('x'), this.get('y'), 2)
+                            .attr("fill", "#dd0").attr("stroke", "#000"));
+  },
+  move: function(){
+    this.set('rot', this.get('rot') + 0.05);
+  }
+});
 
 App.ready = function(){
   App.sun.draw();
   App.earth.draw();
+  App.moon.draw();
 
   setInterval(function(){ App.earth.move(); }, 100);
+  setInterval(function(){ App.moon.move(); }, 80);
 };
 
 //App.ApplicationController = Ember.Controller.extend();
